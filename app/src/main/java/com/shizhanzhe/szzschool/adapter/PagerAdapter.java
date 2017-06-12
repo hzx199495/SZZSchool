4615311c2960eb5628f01c208e977c34a6d077cb
HanZhenxiong <729900236@qq.com>
2017-06-12 14:41:52 +0800

    6.12上传

---
+++ app/src/main/java/com/shizhanzhe/szzschool/adapter/PagerAdapter.java
@@ -0,0 +1,29 @@
+package com.shizhanzhe.szzschool.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by zz9527 on 2017/2/10.
+ */
+
+public class PagerAdapter extends FragmentPagerAdapter {
+    private ArrayList<Fragment> list;
+    public PagerAdapter(FragmentManager fm, ArrayList<Fragment> list) {
+        super(fm);
+        this.list=list;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+}
---
+++ app/src/main/java/com/shizhanzhe/szzschool/fragment/KCFragment.java
@@ -0,0 +1,68 @@
+package com.shizhanzhe.szzschool.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+import com.google.gson.Gson;
+import com.shizhanzhe.szzschool.Bean.ProBean;
+import com.shizhanzhe.szzschool.R;
+import com.shizhanzhe.szzschool.activity.MyApplication;
+import com.shizhanzhe.szzschool.adapter.GVAdapter;
+import com.shizhanzhe.szzschool.adapter.TGAdapter;
+import com.shizhanzhe.szzschool.utils.OkHttpDownloadJsonUtil;
+import com.shizhanzhe.szzschool.utils.Path;
+
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
+import org.xutils.x;
+
+import java.util.List;
+
+/**
+ * Created by zz9527 on 2017/6/12.
+ */
+@ContentView(R.layout.fragment_kc)
+public class KCFragment extends Fragment {
+    @ViewInject(R.id.gv)
+    GridView gv;
+    public static KCFragment newInstance(int type) {
+
+        Bundle args = new Bundle();
+        args.putInt("type",type);
+        KCFragment fragment = new KCFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return x.view().inject(this, inflater, null);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Bundle bundle = getArguments();
+        final int type = bundle.getInt("type");
+        OkHttpDownloadJsonUtil.downloadJson(getActivity(), Path.CENTER(MyApplication.myid, MyApplication.token), new OkHttpDownloadJsonUtil.onOkHttpDownloadListener() {
+            @Override
+            public void onsendJson(String json) {
+                Gson gson = new Gson();
+                List<ProBean.TxBean> tx = gson.fromJson(json, ProBean.class).getTx();
+                List<ProBean.TgBean> tg = gson.fromJson(json, ProBean.class).getTg();
+                if (type==0){
+
+                   gv.setAdapter(new TGAdapter(tg,getContext()));
+                }else if (type==1){
+                    gv.setNumColumns(2);
+                    gv.setAdapter(new GVAdapter(tx, getContext()));
+                }
+            }
+        });
+    }
+}
